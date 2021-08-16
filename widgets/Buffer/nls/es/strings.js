9bc8d881a328d472bb96e1d0032d3d221fadbd95
tytheguy145 <tyler.rourke0145@gmail.com>
2021-08-16 03:28:51 -0400

    Algonquin water
    Algonquin water features

---
+++ widgets/Buffer/nls/es/strings.js
@@ -0,0 +1,22 @@
+define({
+  root: ({
+    _widgetLabel: "Demostración",
+    selectLayer: "Seleccciona una capa",
+    units: "Selecciona una unidad",
+    distanceTextBox: "Distancia",
+    textBoxCoord: "Clica en el mapa o inserta coordenadas",
+    xCoord: "X",
+    yCoord: "Y",
+    executeCoordBuffer: "Ejecutar",
+    intersection: "Selección por intersección",
+    enableSelection: "Activar",
+    disableSelection: "Desactivar",
+    clearSelection: "Limpiar Selección",
+    clearGraphics: "Limpiar",
+    execute: "Ejecutar",
+    xPlaceholder: "-0.48",
+    yPlaceholder: "38.69",
+    distanceValue: "2000",
+    clearBuffers: "Limpiar Buffers"
+  })
+});
